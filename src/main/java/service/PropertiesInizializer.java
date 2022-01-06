dfe4e36233fbf5dad3f4276ce0ba6e1c08037e3a
snipershady <snipershady@gmail.com>
2022-01-06 17:42:02 +0100

    default config demo

--- src/main/java/service/PropertiesInizializer.java
+++ src/main/java/service/PropertiesInizializer.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package service;
 
 import java.util.Properties;
@@ -12,15 +9,19 @@ import java.util.Properties;
  */
 public class PropertiesInizializer {
 
+    /**
+     * Create a new config.properties files with standard required params
+     * @return boolean
+     */
     public boolean init() {
         Properties p = new Properties();
         p.setProperty("db_user", "sa");
-        p.setProperty("db_pass", "");
-        p.setProperty("db_host", "");
+        p.setProperty("db_pass", "WRITE_YOUR_PASSWORD");
+        p.setProperty("db_host", "IP_OF_THE_SQL_SERVER");
         p.setProperty("db_port", "1433");
         p.setProperty("db_name", "master");
-        p.setProperty("server_path", "");
-        p.setProperty("backuplist", "");
+        p.setProperty("server_path", "PATH_TO_SAVE_BACKUPS");
+        p.setProperty("backuplist", "LIST_OF_DB_TO_BACKUP____LEAVE_IT_BLANK_TO_USE_CLI_ARGS***");
 
         PropertiesParser pp = new PropertiesParser();
         return pp.initAllRequiredParameters(p);
